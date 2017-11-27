a10ddd573680fdf1f15f49884a461c14ed6b7f7d
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2017-11-27 00:11:54 -0700

    this is a commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,16 @@
 
 // Generate a random number between 19-120 to display as target number
 
-// Generate a set of four random numbers to be 2
+// Generate a set of four random numbers that are between 1-12 to be assigned to each button
+
+// The target number will be displayed in target number box
+
+// If the player correctly guesses target number, then the player wins
+
+// If the player exeeds the target value of the target number, the player loses
+
+// 
+
+
+ Math.floor(Math.random() * 20); 
+ console.log()
