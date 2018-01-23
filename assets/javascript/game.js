774348c6f9e7c370bbce6d33cc9fb6123829c342
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2018-01-23 17:50:40 -0500

    latest commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-// This is a comment
-
-// Generate a random number between 19-120 to display as target number
-
 // Generate a set of four random numbers that are between 1-12 to be assigned to each button
 
 // The target number will be displayed in target number box
@@ -11,7 +7,39 @@
 // If the player exeeds the target value of the target number, the player loses
 
 // 
+var buttonValues
+var targetValue
+var gemOne = (Math.random() * 120) + 19;
+var gemTwo = (Math.random() * 120) + 19;
+var gemThree = (Math.random() * 120) + 19;
+var gemFour = (Math.random() * 120) + 19;
+
+//Store Data in an attribute
+
+// Generate a random number between 19-120 to display as target number
+$("#targetNumberBox").append(Math.floor(Math.random() * 120) + 19);
 
 
- Math.floor(Math.random() * 20); 
- console.log()
+$(document).ready(function() {
+
+    $("#gem1").click(function() {
+        $("#currentValueBox").text("save")
+    });
+
+    $("#gem2").click(function() {
+        $("#currentValueBox").text("save")
+    });
+
+    $("#gem3").click(function() {
+        $("#currentValueBox").text("save")
+    });
+
+    $("#gem4").click(function() {
+        $("#currentValueBox").text("save")
+    });
+
+});
+
+
+
+// Generate a set of four random numbers that are between 1-12 to be assigned to each button
